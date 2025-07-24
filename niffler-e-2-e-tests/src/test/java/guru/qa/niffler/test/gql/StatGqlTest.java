9b7bee4441054bb36643ea48e9d1716acbe1f994
yulia.pryania <yulia.pryania@gmail.com>
2025-07-24 23:22:44 +0400

    add user controller, stat controller tests

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/gql/StatGqlTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/gql/StatGqlTest.java
@@ -4,18 +4,19 @@ import com.apollographql.apollo.api.ApolloResponse;
 import com.apollographql.java.client.ApolloCall;
 import com.apollographql.java.rx2.Rx2Apollo;
 import guru.qa.StatQuery;
-import guru.qa.niffler.jupiter.annotation.ApiLogin;
-import guru.qa.niffler.jupiter.annotation.Token;
-import guru.qa.niffler.jupiter.annotation.User;
+import guru.qa.niffler.common.values.CurrencyValues;
+import guru.qa.niffler.jupiter.annotation.*;
 import org.junit.jupiter.api.Test;
 
+import static guru.qa.type.CurrencyValues.EUR;
+import static guru.qa.type.CurrencyValues.USD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StatGqlTest extends BaseGqlTest {
   @User
   @ApiLogin
   @Test
-  void allCurrenciesShouldBeReturnedFromCategory(@Token String token) {
+  void newUserStatShouldBeEmpty(@Token String token) {
     ApolloCall<StatQuery.Data> statCall = apolloClient
         .query(StatQuery.builder()
             .statCurrency(null)
@@ -26,6 +27,90 @@ public class StatGqlTest extends BaseGqlTest {
     ApolloResponse<StatQuery.Data> dataApolloResponse = Rx2Apollo.single(statCall).blockingGet();
     final StatQuery.Data data = dataApolloResponse.dataOrThrow();
     StatQuery.Stat result = data.stat;
+
     assertThat(result.total).isEqualTo(0.0);
+  }
+
+  @User(
+      spends = {
+          @Spend(category = "Grocery", description = "Bread", amount = 100.00, currency = CurrencyValues.RUB),
+          @Spend(category = "DIY", description = "Hammer", amount = 30, currency = CurrencyValues.USD),
+          @Spend(category = "Clothes", description = "Jeans", amount = 100, currency = CurrencyValues.USD),
+          @Spend(category = "Footwear", description = "Sneakers", amount = 99.10, currency = CurrencyValues.EUR)
+      }
+  )
+  @ApiLogin
+  @Test
+  void shouldReturnTotalForUsdSpendsWhenMultipleCurrenciesPresent(@Token String token) {
+    ApolloCall<StatQuery.Data> statCall = apolloClient
+        .query(StatQuery.builder()
+            .filterCurrency(USD)
+            .statCurrency(USD)
+            .build())
+        .addHttpHeader("authorization", token);
+
+    ApolloResponse<StatQuery.Data> dataApolloResponse = Rx2Apollo.single(statCall).blockingGet();
+    final StatQuery.Data data = dataApolloResponse.dataOrThrow();
+    StatQuery.Stat result = data.stat;
+
+    assertThat(result.total).isEqualTo(130.0);
+  }
+
+  @User(
+      spends = {
+          @Spend(category = "Grocery", description = "Bread", amount = 100.00, currency = CurrencyValues.RUB),
+          @Spend(category = "DIY", description = "Hammer", amount = 30, currency = CurrencyValues.USD),
+          @Spend(category = "Clothes", description = "Jeans", amount = 100, currency = CurrencyValues.USD),
+          @Spend(category = "Footwear", description = "Sneakers", amount = 99.10, currency = CurrencyValues.EUR)
+      }
+  )
+  @ApiLogin
+  @Test
+  void shouldReturnTotalForEurSpendsWhenMultipleCurrenciesPresent(@Token String token) {
+    ApolloCall<StatQuery.Data> statCall = apolloClient
+        .query(StatQuery.builder()
+            .filterCurrency(EUR)
+            .statCurrency(EUR)
+            .build())
+        .addHttpHeader("authorization", token);
+
+    ApolloResponse<StatQuery.Data> dataApolloResponse = Rx2Apollo.single(statCall).blockingGet();
+    final StatQuery.Data data = dataApolloResponse.dataOrThrow();
+    StatQuery.Stat result = data.stat;
+
+    assertThat(result.total).isEqualTo(99.10);
+  }
+
+  @User(
+      spends = {
+          @Spend(category = "Grocery", description = "Bread", amount = 100.00),
+          @Spend(category = "Footwear", description = "Sneakers", amount = 99.10),
+          @Spend(category = "DIY", description = "Hammer", amount = 599.10)
+      },
+      categories = {
+          @Category(name = "DIY", archived = true),
+          @Category(name = "Footwear", archived = true)
+      }
+  )
+  @ApiLogin
+  @Test
+  void shouldReturnTotalForArchivedSpends(@Token String token) {
+    ApolloCall<StatQuery.Data> statCall = apolloClient
+        .query(StatQuery.builder()
+            .filterCurrency(null)
+            .statCurrency(null)
+            .build())
+        .addHttpHeader("authorization", token);
+
+    ApolloResponse<StatQuery.Data> dataApolloResponse = Rx2Apollo.single(statCall).blockingGet();
+    final StatQuery.Data data = dataApolloResponse.dataOrThrow();
+    StatQuery.Stat result = data.stat;
+
+    assertThat(result.statByCategories)
+        .hasSize(2)
+        .anySatisfy(cat -> {
+          assertThat(cat.categoryName).isEqualTo("Archived");
+          assertThat(cat.sum).isEqualTo(698.2);
+        });
   }
 }
